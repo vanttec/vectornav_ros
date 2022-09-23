1d9fb29c34be069b5aab841fe4815a46889495e0
DRogerSC <musicfan211@hotmail.com>
2022-09-23 17:38:39 -0500

    Removed redundant class attribute, calculated quaternion orientation using tf2/quaternion library, fixed code standard

--- src/path_publisher.cpp
+++ src/path_publisher.cpp
@@ -1,8 +1,22 @@
+/*
+@file :        path_publisher.cpp
+@date:         Fri Sep 23, 2022
+@date_modif:   Fri Sep 23, 2022
+@author:       David Rogelio Saláis Carrasco
+@e-mail:	   rogelio.salais@hotmail.com
+@brief:		   This script publishes a path topic for visualization in RVIZ 
+				directly from NED_pose information obtained from the VN-3OO sensor
+				For use in the SDV project
+@version:
+Copyright		Open Source 
+*/
+
 #include <iostream>
 #include <math.h>
 #include <vector>
 #include "ros/ros.h"	
 #include "ros/time.h"
+#include "tf2/LinearMath/Quaternion.h"
 #include "nav_msgs/Path.h"
 #include "geometry_msgs/Pose2D.h"
 #include "geometry_msgs/Vector3.h"
@@ -13,115 +27,86 @@
 #include "std_msgs/Float64.h"
 #include "std_msgs/UInt8.h"
 
-using namespace std;
 
-class sdvPathNodeClass
+class SdvPathNodeClass
 {
 	public:
-		geometry_msgs::Pose2D receivedPose;
-		geometry_msgs::Pose poseToAppend;
-		geometry_msgs::PoseStamped stampedPoseToAppend;
-		nav_msgs::Path pathToPublish;
-		uint32_t pathSeq;
-		uint32_t poseSeq;
+		geometry_msgs::Pose2D received_pose;
+		geometry_msgs::PoseStamped stamped_pose_to_append;
+		nav_msgs::Path path_to_publish;
+		tf2::Quaternion *auxiliary_quaternion;
+	
+	void initializePath(){
+		sdv_path_pub = n.advertise<nav_msgs::Path>("/vectornav/rviz_data/path", 1000);
+		sdv_ned_pose_sub = n.subscribe("/vectornav/ins_2d/NED_pose", 1000, &SdvPathNodeClass::nedPoseCallback, this);
+		path_to_publish.header.frame_id = "world";
+		path_to_publish.header.stamp = ros::Time::now();
+		path_to_publish.poses.reserve(100*sizeof(geometry_msgs::PoseStamped));
+	}
+
+
+	void nedPoseCallback(const geometry_msgs::Pose2D::ConstPtr& msg){
 		
+		received_pose.theta = msg -> theta;
+		received_pose.x = msg -> x;
+		received_pose.y = msg -> y;
+		auxiliary_quaternion -> setRPY(3.1415926535, 0, -msg->theta);
+		
+	}
+
+
+	void updatePath()
+	{
+		path_to_publish.header.seq = path_counter++;
+		path_to_publish.header.stamp = ros::Time::now();
+		path_to_publish.poses.push_back(stamped_pose_to_append);
+		sdv_path_pub.publish(path_to_publish);
+	}
 
 	
-
-	void initialize_path_to_publish(){
-		sdv_path_pub = n.advertise<nav_msgs::Path>("/vectornav/rviz_data/path", 1000);
-		sdv_ned_pose_sub = n.subscribe("/vectornav/ins_2d/NED_pose", 1000, &sdvPathNodeClass::NED_pose_callback, this);
-
-
-		pathToPublish.header.frame_id = "world";
-		pathSeq = 0;
-		poseSeq = 0;
-		pathToPublish.header.stamp = ros::Time::now();
-		pathToPublish.poses.reserve(100*sizeof(geometry_msgs::PoseStamped));
+	void obtainAndStampPose(){
+		stamped_pose_to_append.pose.orientation.w = 1;
+		stamped_pose_to_append.pose.orientation.x = auxiliary_quaternion -> getX();
+		stamped_pose_to_append.pose.orientation.y = auxiliary_quaternion -> getY();
+		stamped_pose_to_append.pose.orientation.z = auxiliary_quaternion -> getZ();
+		stamped_pose_to_append.pose.position.z = 0;
+		stamped_pose_to_append.pose.position.x = received_pose.x;
+		stamped_pose_to_append.pose.position.y = -received_pose.y;
+		stamped_pose_to_append.header.frame_id = "world";
+		stamped_pose_to_append.header.seq = pose_counter++;
+		stamped_pose_to_append.header.stamp = ros::Time::now(); 
 	}
 
-
-	void NED_pose_callback(const geometry_msgs::Pose2D::ConstPtr& _msg){
-		
-		
-		receivedPose.theta = _msg -> theta;
-		receivedPose.x = _msg -> x;
-		receivedPose.y = _msg -> y;
-		
-	}
-
-
-	void update_path()
-	{
-		pathToPublish.header.seq = pathSeq++;
-		pathToPublish.header.stamp = ros::Time::now();
-		pathToPublish.poses.push_back(stampedPoseToAppend);
-		sdv_path_pub.publish(pathToPublish);
-	}
-
-	void obtain_pose(){
-		poseToAppend.orientation.w = 0;
-		poseToAppend.orientation.x = 0;
-		poseToAppend.orientation.y = 0;
-		poseToAppend.orientation.z = 0;
-		poseToAppend.position.z = 0;
-
-
-		poseToAppend.position.x = receivedPose.x;
-		poseToAppend.position.y = -receivedPose.y;
-	}
-
-	void stamp_pose(){
-		stampedPoseToAppend.pose = poseToAppend;
-		stampedPoseToAppend.header.frame_id = "world";
-		stampedPoseToAppend.header.seq = poseSeq++;
-		stampedPoseToAppend.header.stamp = ros::Time::now(); 
-	}
 
 	private:
 		ros::NodeHandle n;
 		ros::Publisher sdv_path_pub;
 		ros::Subscriber sdv_ned_pose_sub;
+		uint32_t path_counter = 0;
+		uint32_t pose_counter = 0;
 
 };
 
 int main(int argc, char *argv[])
 {
-	/**
-	* The ros::init() function needs to see argc and argv so that it can perform
-	* any ROS arguments and name remapping that were provided at the command line.
-	* For programmatic remappings you can use a different version of init() which takes
-	* remappings directly, but for most command-line programs, passing argc and argv is
-	* the easiest way to do it.  The third argument to init() is the name of the node.
-	*
-	* part of the ROS system.
-	*/
+
 	ros::init(argc, argv, "imu_path_publisher");
 	ros::Time::init();
-
-	/*
-	 *  The nodehandle fully initializes the node; when it is destroyed, it ends
-	 */ 
-
-
 	ros::Rate loop_rate(100);
 
 	//Creation of initial path and frame
 
-	sdvPathNodeClass sdvPathNode;
-	sdvPathNode.initialize_path_to_publish();
+	SdvPathNodeClass sdvPathNode;
+	sdvPathNode.initializePath();
 
 
 
   while (ros::ok())
   {
     ros::spinOnce();
-	sdvPathNode.obtain_pose();
-	sdvPathNode.stamp_pose();
-	sdvPathNode.update_path();
+	sdvPathNode.obtainAndStampPose();
+	sdvPathNode.updatePath();
     loop_rate.sleep();
   }
-
-
 	return 0;
 }
